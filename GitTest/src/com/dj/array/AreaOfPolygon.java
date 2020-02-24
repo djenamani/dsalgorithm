22ce2d6453425984358a17bedebc6d4fd126d55e
djenaman <djenaman@N-20HEPF11XSSN.nsn-intra.net>
2020-02-24 12:08:46 +0530

    Area of polygon

---
+++ GitTest/src/com/dj/array/AreaOfPolygon.java
@@ -0,0 +1,18 @@
+package com.dj.array;
+
+public class AreaOfPolygon {
+
+	
+	static int shapeArea(int n) {
+	    // the formula for calculating the area of polygon is
+	    // n**2 + (n-1)**2
+	    // return 1 if n = 1
+		return (n == 1) ? 1 : (int)Math.pow(n, 2) + (int)Math.pow(n-1, 2);
+	}
+
+	
+	public static void main(String[] args) {
+		int area = shapeArea(3);
+		System.out.println(area);
+	}
+}
